4dc1b7d24014ea4715be93a8eaa79398ce55107d
Savely <savely.jadan@gmail.com>
2025-02-08 11:00:13 +0700

    Add checkboxes component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, TopBar } from '@/components/shared'
+import { Container, Filters, Title, TopBar } from '@/components/shared'
 
 export default function Home() {
     return (
@@ -7,6 +7,13 @@ export default function Home() {
                 <Title text="Все пиццы" size="lg" className="font-extrabold" />
             </Container>
             <TopBar />
+            <Container className="pb-14 mt-9">
+                <div className="flex gap-[60px]">
+                    <div className="w-[250px]">
+                        <Filters />
+                    </div>
+                </div>
+            </Container>
         </>
     )
 }
